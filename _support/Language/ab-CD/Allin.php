6b842e41d893b31118875217cb70e9004e51d311
Jim Parry <jim_parry@telus.net>
2019-10-15 09:01:37 -0700

    Remove _support in project root

--- _support/Language/ab-CD/Allin.php
+++
@@ -1,8 +0,0 @@
-<?php
-// seven wonders of the ancient world
-return [
-   'one' => 'Pyramid of Giza',
-   'tre' => 'Colossus of Rhodes',
-   'fiv' => 'Temple of Artemis',
-   'sev' => 'Hanging Gardens of Babylon',
-];
--- _support/Language/en-ZZ/More.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-   'strongForce' => 'These are not the droids you are looking for',
-   'notaMoon'    => "It's made of cheese",
-   'wisdom'      => 'There is no try',
-];
