cd78a645bec8ff642ae32caf0b60abb32957b6ff
Darryl Crockett <darryl_crockett1@hotmail.com>
2020-07-14 00:21:35 -0400

    starting adminLTE

--- src/constants/index.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-    "projectInfo": {
-        "appName": "Admin Dashboard",
-        "hostName": "vanguard-trucking.com"
-    },
-    "host": "https://vanguard-trucking.com",
-    "devHost": "http://localhost:8888/"
-}
--- src/views/Dashboard/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Budget } from './Budget';
-export { default as RecentLoads } from './RecentLoads';
-export { default as LatestProducts } from './LatestProducts';
-export { default as LatestSales } from './LatestSales';
-export { default as TasksProgress } from './TasksProgress';
-export { default as TotalProfit } from './TotalProfit';
-export { default as TotalUsers } from './TotalUsers';
-export { default as UsersByDevice } from './UsersByDevice';
