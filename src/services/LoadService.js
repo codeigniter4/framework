52e2f57a86bfbd7dc145f9c1e6b29460a006e609
Darryl Crockett <darryl_crockett1@hotmail.com>
2020-07-28 22:49:24 -0400

    deployed to gcloud

--- src/services/LoadService.js
+++ src/services/LoadService.js
@@ -1,18 +1,18 @@
 export const getLoads = async () => {
-  const response = await fetch('http://vanguard-trucking.com/api/loads')
+  const response = await fetch('https://vanguard-trucking.com/api/loads')
   const json = await response.json();
   return json;
 }
 
 
 export const getLoadByID = async (id) => {
-  const response = await fetch('http://vanguard-trucking.com/api/loads/id/' + id);
+  const response = await fetch('https://vanguard-trucking.com/api/loads/id/' + id);
   const json = await response.json();
   return json;
 }
 
 export const saveLoad = async (load) => {
-  const response = await fetch('http://vanguard-trucking.com/api/loads', {
+  const response = await fetch('https://vanguard-trucking.com/api/loads', {
     method: 'post',
     body: JSON.stringify(load)
   })
@@ -26,7 +26,7 @@ export const saveLoad = async (load) => {
 
 
 export const deleteLoadById = async (id) => {
-  const response = await fetch('http://vanguard-trucking.com/api/loads/delete/' + id, {
+  const response = await fetch('https://vanguard-trucking.com/api/loads/delete/' + id, {
     method: 'post'
   });
   const json = await response.json();
