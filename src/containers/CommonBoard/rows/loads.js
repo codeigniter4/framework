9bc0a2437bbeadef27bd331c9b95ce3404b50adb
Darryl Crockett <darryl_crockett1@hotmail.com>
2020-10-17 00:31:48 -0400

    added btn

--- src/containers/CommonBoard/rows/loads.js
+++ src/containers/CommonBoard/rows/loads.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from '@material-ui/core';
 import Link from '@material-ui/core/Link';
 const editBrokerButton = (loadId, brokerId, actions, name) => (<Link href="#" onClick={(e) => actions.handleBrokerClick(e, loadId, brokerId)}>{name}</Link>);
-
+const addBrokerButton = (loadId, brokerId, actions, name) => (<Button color="secondary" size="small" variant="contained" onClick={(e) => actions.handleBrokerClick(e, loadId, brokerId)}>Add</Button>)
 
 export const getLoadRowData = (context, rows, actions, editButton) => {
   const { tableData } = context;
@@ -10,7 +10,7 @@ export const getLoadRowData = (context, rows, actions, editButton) => {
   return rows.map(row => {
     const newRow = {...row};
     newRow.edit = editButton(row.id, actions);
-    newRow.editBroker = editBrokerButton(row.id, 'addNew', actions, 'Add New');
+    newRow.editBroker = addBrokerButton(row.id, 'addNew', actions, 'Add');
 
     if(tableData.brokers && tableData.brokers.length) {
       tableData.brokers.map(broker => {
