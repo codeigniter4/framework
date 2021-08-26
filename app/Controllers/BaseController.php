8b7847882835743914ccc1f08e5b8387492b59f7
Reza Nova Pradito <pradito30@gmail.com>
2021-08-26 08:43:11 +0700

    tes1

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -54,6 +54,6 @@ class BaseController extends Controller
 		// Preload any models, libraries, etc, here.
 		//--------------------------------------------------------------------
 		// E.g.: $this->session = \Config\Services::session();
-		//tess
+		//tess1
 	}
 }
