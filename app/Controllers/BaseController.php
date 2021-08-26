5d8705589a77688e9dc1ff6723cf89c40632391f
Reza Nova Pradito <pradito30@gmail.com>
2021-08-26 08:36:46 +0700

    commit1

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -36,7 +36,7 @@ class BaseController extends Controller
 	 *
 	 * @var array
 	 */
-	//protected $helpers = [];
+	protected $helpers = [];
 
 	/**
 	 * Constructor.
@@ -54,6 +54,6 @@ class BaseController extends Controller
 		// Preload any models, libraries, etc, here.
 		//--------------------------------------------------------------------
 		// E.g.: $this->session = \Config\Services::session();
-		//tes
+		//tess
 	}
 }
