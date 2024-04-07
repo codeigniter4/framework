72d68f8d46a99a2d3b82071c0f81d35a82a1e48f
Balagan <balaganengineering@gmail.com>
2024-04-07 14:30:32 +0530

    Expose PHP Version

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -225,7 +225,7 @@
 
     <div class="heroe">
 
-        <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
+        <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?> on <?= phpversion()?></h1>
 
         <h2>The small framework with powerful features</h2>
 
